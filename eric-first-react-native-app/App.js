6ec6075a8b36f4f8f9e9b5330021effbb9941dee
Mohammad Askari (Eric) <ericaskari@gmail.com>
2023-01-11 12:03:57 +0200

    http-a stuff DONE

--- eric-first-react-native-app/App.js
+++ eric-first-react-native-app/App.js
@@ -1,12 +1,11 @@
 import {StyleSheet, SafeAreaView, StatusBar} from 'react-native';
 import {List} from './components/List';
-import {mediaArray} from './datasource';
 
 const App = () => {
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <List items={mediaArray} />
+        <List />
       </SafeAreaView>
       <StatusBar style="auto" />
     </>
--- eric-first-react-native-app/components/List.js
+++ eric-first-react-native-app/components/List.js
@@ -1,9 +1,40 @@
-import {mediaArray} from '../datasource';
 import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
 import {ListItem} from './ListItem';
+import {useEffect, useState} from 'react';
 
-export const List = ({items}) => {
+export const List = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setHasError(false);
+
+    (async () => {
+      try {
+        const url =
+          'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setMediaArray(json);
+        setIsLoading(false);
+        setHasError(false);
+      } catch (e) {
+        setMediaArray([]);
+        setIsLoading(false);
+        setHasError(true);
+        console.log(e);
+      }
+    })();
+  }, []);
   return (
-    <FlatList data={items} renderItem={({item}) => <ListItem item={item} />} />
+    <FlatList
+      data={mediaArray}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({item}) => <ListItem item={item} />}
+    />
   );
 };
