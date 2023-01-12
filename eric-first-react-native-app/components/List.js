4d2ea39b6dce02794ac981cbd733e31909a7bd09
Mohammad Askari (Eric) <ericaskari@gmail.com>
2023-01-12 11:22:24 +0200

    Styling

--- eric-first-react-native-app/components/List.js
+++ eric-first-react-native-app/components/List.js
@@ -1,9 +1,72 @@
 import {mediaArray} from '../datasource';
 import {FlatList, Image, Text, TouchableOpacity, View} from 'react-native';
 import {ListItem} from './ListItem';
+import {useAssets} from 'expo-asset';
 
 export const List = ({items}) => {
+  const [assets, error] = useAssets([
+    require('../assets/kitten.jpg'),
+    require('../assets/settings.png'),
+  ]);
+  console.log({error, assets});
   return (
-    <FlatList data={items} renderItem={({item}) => <ListItem item={item} />} />
+    <View>
+      <FlatList
+        style={{backgroundColor: '#202028'}}
+        data={[true, ...items]}
+        renderItem={({item, index}) =>
+          index === 0 ? (
+            <View
+              style={{
+                width: '100%',
+                borderRadius: '30px',
+              }}
+            >
+              {assets ? (
+                <>
+                  <Image
+                    source={assets[0]}
+                    style={{
+                      resizeMode: 'cover',
+                      width: '100%',
+                    }}
+                  />
+                  <Image
+                    source={assets[1]}
+                    style={{
+                      position: 'absolute',
+                      top: 30,
+                      resizeMode: 'contain',
+                      right: 30,
+                      height: 30,
+                      width: 30,
+                      fontSize: 30,
+                      color: 'white',
+                      fill: 'white',
+                    }}
+                  />
+                </>
+              ) : (
+                <></>
+              )}
+              <Text
+                style={{
+                  position: 'absolute',
+                  bottom: 20,
+                  fontSize: 30,
+                  backgroundColor: '#2669FD',
+                  color: 'white',
+                  padding: 10,
+                }}
+              >
+                Homeless Kittens
+              </Text>
+            </View>
+          ) : (
+            <ListItem item={item} />
+          )
+        }
+      />
+    </View>
   );
 };
--- eric-first-react-native-app/components/ListItem.js
+++ eric-first-react-native-app/components/ListItem.js
@@ -1,33 +1,55 @@
-import {Image, Text, TouchableOpacity, View} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import PropTypes from 'prop-types';
 
 export const ListItem = ({item}) => {
   return (
-    <TouchableOpacity style={{display: 'flex'}}>
+    <TouchableOpacity style={{display: 'flex', padding: 10}}>
       <View
         style={[
           {
             flexDirection: 'row',
-            backgroundColor: 'gray',
+            backgroundColor: '#242834',
             padding: 10,
             marginVertical: 5,
+            display: 'flex',
           },
         ]}
       >
-        <View style={[{flexDirection: 'row', padding: 10}]}>
+        <View style={[{flex: 1, padding: 10}]}>
           <Image
-            style={{width: 100, height: 100}}
+            style={styles.cardImage}
             source={{uri: item.thumbnails.w160}}
           />
         </View>
-        <View style={[{flexDirection: 'column'}]}>
-          <Text style={[{fontSize: '22px'}]}>{item.title}</Text>
-          <Text>{item.description}</Text>
+        <View style={[{flexDirection: 'column', flex: 1, paddingVertical: 20}]}>
+          <Text style={styles.titleText}>{item.title}</Text>
+          <Text style={styles.descriptionText}>{item.description}</Text>
         </View>
       </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cardImage: {
+    flex: 1,
+    fontSize: '22px',
+    color: 'rgb(255,255,255)',
+    borderRadius: '10px',
+    borderBottomLeftRadius: '60px',
+  },
+  titleText: {
+    fontSize: '22px',
+    color: 'rgb(255,255,255)',
+  },
+  descriptionText: {
+    color: 'rgb(134,134,134)',
+  },
+});
+
 ListItem.propTypes = {
   item: PropTypes.object,
 };
